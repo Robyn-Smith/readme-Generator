9d1ccdff82da1c4da61122ecbd25ca4f392fa94b
Robyn Smith <robynsmith21@hotmail.com>
2023-06-09 22:46:52 +0100

    added packages needed for app

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const prompts = require ('./utils/prompts');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(markdown) {
+          fs.writeFile('readme.md', markdown, (err) =>
+  err ? console.error(err) : console.log('new file created')
+);}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(prompts)
+    .then((response)) => {
+        const  md = generateMarkdown(response);
+        writeToFile(markdown);
+    }
+}
 
 // Function call to initialize app
 init();
+
+
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       message: 'What is your user name?',
+//       name: 'username',
+//     },
+//     {
+//       type: 'input',
+//       message: 'What languages do you know?',
+//       name: 'language',
+//     },
+//     {
+//       type: 'input',
+//       message: 'What is your preferred method of communication?',
+//       name: 'communication',
+//     },
+//   ])
+//   .then((response) => {
+//     response.confirm === response.password
+//       ? console.log('Success!')
+//       : console.log('something is incorrect')
+//       fs.writeFile('log.txt', `${response.username}`, (err) =>
+//   err ? console.error(err) : console.log('new file')
+// );}
+//   );
