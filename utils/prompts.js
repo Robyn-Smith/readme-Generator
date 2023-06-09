4101f64ce902b2c933ac2e074bee3cb373c8a07b
Robyn Smith <robynsmith21@hotmail.com>
2023-06-09 22:53:27 +0100

    added seperate prompts.js

--- utils/prompts.js
+++ utils/prompts.js
@@ -1,1 +1,17 @@
-const prompts = [];
+const prompts = [
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'What languages do you know?',
+      name: 'language',
+    },
+    {
+      type: 'input',
+      message: 'What is your preferred method of communication?',
+      name: 'communication',
+    },
+];
