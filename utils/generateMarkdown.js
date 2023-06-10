339bddaf1e430241169ad1d81b8105d9067a6342
Robyn Smith <robynsmith21@hotmail.com>
2023-06-10 03:36:38 +0100

    started generate markdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,17 +9,43 @@ function renderLicenseLink(license) {}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if ()
-}
+  if (license == 'none'){
+    return
+  }
+  return '';
+}//not finished
+
 function renderLicenseBadge(license) {
   if (license !== 'None') {
     return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   }
   return '';
 }
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+
+  ## Description
+  ${}
+
+  ## Table of Contents
+  *[Description](#description)
+  *[Installation] (#installation)
+  *[Usage] (#usage)
+  *[Contributions] (#contributions)
+  *[License] (#license)
+  *[Testing] (#tests)
+
+
+  ##Installation
+  ${}
+
+
+  ##Usage
+  ${}
+
 
 `;
 }
