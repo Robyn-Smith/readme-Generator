f1ee16e0fd1dc8505e263fbb939a23c2ed445a8e
Robyn Smith <robynsmith21@hotmail.com>
2023-06-10 03:14:01 +0100

    added badge license code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,8 +8,15 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if ()
+}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
